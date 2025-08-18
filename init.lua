9999e5807bafac1dc5a0a3b0f17fc92ec4df4d3a
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-08-18 07:38:11 +0530

    s

--- init.lua
+++ init.lua
@@ -50,10 +50,18 @@ vim.pack.add({
 	{ src = "https://github.com/stevearc/conform.nvim" },
 	{ src = "https://github.com/echasnovski/mini.pick" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/windwp/nvim-autopairs", event = "InsertEnter" },
+	{ src = "https://github.com/hrsh7th/cmp-nvim-lsp" },
+	{ src = "https://github.com/L3MON4D3/LuaSnip" },
+	{ src = "https://github.com/saadparwaiz1/cmp_luasnip" },
+	{ src = "https://github.com/rafamadriz/friendly-snippets" },
+	{ src = "https://github.com/hrsh7th/nvim-cmp" },
 })
 
 require("mini.pick").setup()
 require("oil").setup()
+require("nvim-autopairs").setup({})
+
 -- Color Scheme
 require("onedark").setup({
 	style = "warmer",
@@ -145,6 +153,33 @@ vim.lsp.enable({
 	"cssls",
 	"jsonls",
 	"eslint",
+})
+
+local cmp = require("cmp")
+require("luasnip.loaders.from_vscode").lazy_load()
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+	}, {
+		{ name = "buffer" },
+	}),
 })
 
 require("conform").setup({
