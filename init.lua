5f2f55ab0e350015fce2cd0456497c6445274d30
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-08-18 07:16:43 +0530

    s

--- init.lua
+++ init.lua
@@ -136,7 +136,7 @@ require("nvim-treesitter.configs").setup({
 	},
 })
 
-vim.lsp.enable({ "lua_ls", "gopls" })
+vim.lsp.enable({ "lua_ls", "gopls", "rust_analyzer", "pyright" })
 
 require("conform").setup({
 	formatters_by_ft = {
