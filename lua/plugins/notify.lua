b769f2acd96b0148ef621b170e331d9abb741b83
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-08-22 04:26:41 +0530

    s

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,7 +1,6 @@
 return {
-	"rcarriga/nvim-notify",
-	event = "VeryLazy",
+	"vigoux/notifier.nvim",
 	config = function()
-		require("notify").setup({ background_colour = "#000000" })
+		require("notifier").setup({})
 	end,
 }
